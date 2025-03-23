3f1467620c72decf0f35916d0eb5d04606a13b37
Thiago Chagras <thiago.chagras@fatec.sp.gov.br>
2025-03-23 15:17:26 -0300

    learn(ap07-react): encerra a seção 2.2

---
+++ exibe-fotos/src/App.jsx
@@ -0,0 +1,14 @@
+//rafce
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Exibe uma lista de...</h1>
+      <i className="pi pi-search"></i>
+      <i className="pi pi-linkedin"></i>
+    </div>
+  )
+}
+
+export default App
