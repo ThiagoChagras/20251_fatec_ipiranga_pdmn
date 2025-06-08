fc7defe8bacc9bdd9a11e12fecf1aecd1dc55081
Thiago Chagras <thiago.chagras@fatec.sp.gov.br>
2025-06-07 22:26:11 -0300

    learn(ap08-react): encerra a apostila
    -encerramos o conteúdo do semetre
    -fazer a prova para a semana que vem

--- hooks-react/src/components/Busca.jsx
+++ hooks-react/src/components/Busca.jsx
@@ -1,15 +1,14 @@
 //rafce
 import axios from 'axios'
-import striptags from 'striptags'
+//import striptags from 'striptags'
 import React, { useState, useEffect } from 'react'
+import {Button } from 'primereact/button'
 import { IconField } from 'primereact/iconfield'
 import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
 
 const Busca = () => {
-  const [termoDeBusca, setTermoBusca] = useState('React')
-  //declarar uma variavel de estado para os resultados
-  //ela deve começar como uma lista vazia
+  const [termoDeBusca, setTermoDeBusca] = useState('React')
   const [resultados, setResultados] = useState([])
 
   useEffect(() => {
@@ -27,8 +26,19 @@ const Busca = () => {
       //conforme a estrutura da resposta devolvida pela wikipedia
       setResultados(data.query.search)
     }
-    fazerBusca ()
-  }, [termoDeBusca])
+    if(termoDeBusca && resultados.length === 0) {
+      fazerBusca()
+    }
+    else{
+      const timeoutID = setTimeout(() => {
+        if(termoDeBusca)
+          fazerBusca ()
+      }, 1000)
+      return () => {
+        clearTimeout(timeoutID)
+      }
+    }
+  }, [resultados.length, termoDeBusca])
 
   return (
     <div>
@@ -36,21 +46,29 @@ const Busca = () => {
             <InputIcon className="pi pi-search"/>
             <InputText 
                 placeholder='Buscar...' 
-                onChange={(e) => {setTermoBusca(e.target.value)}}
+                onChange={(e) => {setTermoDeBusca(e.target.value)}}
                 value={termoDeBusca}/>
         </IconField>
         {/*para cada item, produzir um p que exibe seu snnipet*/}
         {
           resultados.map((resultado) => (
             <div
-              key={resultados.pageid}
+              key={resultado.pageid}
               className='my-2 border border-1 border-400'>
-                <div className="border-bottom border border-1 border-400 font-bold">
+                <div className="border-bottom border border-1 border-400 p-2 text-center font-bold">
                   {resultado.title}
+                  <span>
+                    <Button
+                      icon="pi pi-send"
+                      className='ml-3 p-button-rounded p-button-secondary'
+                      onClick={() => {
+                        window.open(`https://en.wikipedia.org?curid=${resultado.pageid}`)
+                      }}/>
+                  </span>
                 </div>
                 <div className='p-2'>
-                  <span>
-                    dangerouslySetInnerHTML={{__html: resultado.snippet}}
+                  <span
+                    dangerouslySetInnerHTML={{__html: resultado.snippet}}>
                   </span>
                 </div>
             </div>
